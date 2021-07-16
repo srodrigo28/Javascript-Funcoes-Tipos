2bfed9d019f2a23935c0df5aaf46117153e41d30
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2021-07-16 08:10:21 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Javascript-Funcoes-Tipos
---
+++ index.js
@@ -0,0 +1,7 @@
+let nome = 'Sebastião Rodrigo';
+let idade = 20;
+let cadado = true;
+
+function cadastrar(){
+    return alert('cadastrado com sucesso');
+}
