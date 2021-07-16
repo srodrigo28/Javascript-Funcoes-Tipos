df33c968910624926214049d71d7135fcca324b6
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2021-07-16 08:49:24 -0300

    Tipos String

---
+++ type-string.js
@@ -0,0 +1,13 @@
+const texto1 = "Olá, mundo!"
+const texto2 = "Olá mundo!"
+const senha = "senhaSuperSegura456"
+const StringDeNumeros = "3234234"
+
+const meuNome = "Sebastião"
+const citacao = 'O Léo disse "oi!" '
+console.log(citacao)
+
+// Concatenação (+)
+console.log(citacao + meuNome)
+
+// Template Literal
