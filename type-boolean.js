1efba2604cc5a8ed1845ef2b4be3b841b517d022
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2021-07-16 08:59:47 -0300

    Fazendo Comparações

---
+++ type-boolean.js
@@ -0,0 +1,17 @@
+var primeiroNumero = 5;
+var segundoNumero = 5;
+// console.log("----- Comparando Numeros do mesmo tipo -----")
+// console.log( primeiroNumero === segundoNumero )
+// console.log( primeiroNumero == segundoNumero )
+
+primeiroNumero = 5;
+segundoNumero = '5';
+// console.log("----- Comparando Numeros do mesmo tipos difernetes -----")
+// console.log( primeiroNumero === segundoNumero )
+// console.log( primeiroNumero == segundoNumero )
+
+var texto1 = "Alura";
+var texto2 = "alura";
+
+console.log(texto1 === texto2)
+console.log(texto1 == texto2)
